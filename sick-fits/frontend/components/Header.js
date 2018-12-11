f69c778983dfade6141ef74fc9aa96ed3f624ca8
supernova0606 <drivendev66@gmail.com>
2018-12-11 09:26:56 -0600

    added Page component and Header

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => (
+	<div>
+		<div className='bar'>
+			<a href=''>Sick Fits</a>
+		</div>
+		<div className='sub-bar'>
+			<p>Search</p>
+		</div>
+		<div>
+			Cart
+		</div>
+	</div>
+)
+
+export default Header
