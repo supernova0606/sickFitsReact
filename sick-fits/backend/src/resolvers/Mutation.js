ef25f2d8046f38b870a134d8eb919d9cc1ea2976
supernova0606 <drivendev66@gmail.com>
2019-01-05 09:02:38 -0600

    set up Prisma query and mutations, created Items in db

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+	async createItem(parent,args,ctx,info) {
+		// TODO: CHECK TO SEE IF USER IS LOGGED IN
+		const item = await ctx.db.mutation.createItem({
+			data: { ...args}
+		}, info)
 
-module.exports = mutations;
+		return item
+	}
+};
+
+module.exports = Mutations;
